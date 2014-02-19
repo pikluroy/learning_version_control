ae3e5ba219b62f17082845fc52cedd28f4844bd2
Piklu Roy Chowdhury (nee: Bhattacharya) <piklu.roychowdhury@gmail.com>
2014-02-19 12:42:53 +1100

    Ignore some files we dont want to track2

---
+++ gapminder.R
@@ -0,0 +1,8 @@
+data <- read.csv("data/gapminder-FiveYearData.csv", stringsAsFactors=FALSE)
+##MY BIG ANALYSIS
+
+# Subset the data
+
+# Probably need some data to subset
+
+# This will be version controlled
